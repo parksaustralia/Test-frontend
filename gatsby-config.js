f4bdbe91d9bd9b8816f3e29c17b7827dc1ca2b60
Joel Xiong <joel_xiong@yahoo.com.au>
2021-10-11 12:56:19 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-gatsby-cloud`, 
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+	      baseUrl: `https://dev-pa-cms.pantheonsite.io`,
+	    // fastBuilds: true, // has issue with Drupal API key
+	      params: {
+	        pi-key`: process.env.API_KEY,
+	      },
+       },
+    },
   ],
 }
