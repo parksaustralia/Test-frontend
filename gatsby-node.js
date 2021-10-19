4432600d5c346d0acc3eac646e215a17f8bff5b9
Joel Xiong <joel_xiong@yahoo.com.au>
2021-10-19 16:24:38 +1100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+const park = process.env.PARK;
+
+async function createAllPages(createPage, pageTemplate, graphql) {  
+  const result = await graphql(
+    `
+      query {
+        pages: allNodePage(
+          filter: {
+            path: { alias: { ne: null } }
+          }
+        ) {
+          edges {
+            node {
+              path {
+                alias
+              }
+            }
+          }
+        }
+      }
+    `,
+    { park }
+  );    
+
+  result.data.pages.edges.forEach(({ node }) => {
+    createPage({
+      path: node.path.alias,
+      component: pageTemplate,
+      context: {
+        slug: node.path.alias,
+      },
+    });
+  });    
+}
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const pageTemplate = path.resolve(`src/templates/page.tsx`);
+  await createAllPages(createPage, pageTemplate, graphql);
+};
